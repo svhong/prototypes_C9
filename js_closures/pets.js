02443dbb500dfa2f51b7b99e38cfe69d75b243ef
Daniel Paschal <daniel.paschal@learningfuze.com>
2016-07-13 09:12:24 -0700

    added js oop

---
+++ js_closures/pets.js
@@ -0,0 +1,35 @@
+
+var pets = [
+	{
+		name: 'Fluffy',
+		type: 'dog',
+		owner: 'John',
+		personality: 'Super chill with a penchant for eating offal',
+		primary_color: 'brown',
+		secondary_color: 'white'
+	},
+	{
+		name: 'Bubba Joe',
+		type: 'snake',
+		owner: 'Jane',
+		personality: 'Forked tongue, vaguely scaly',
+		primary_color: 'green',
+		secondary_color: 'darkgreen'
+	},
+	{
+		name: 'Maurice',
+		type: 'cat',
+		owner: 'Mary',
+		personality: 'Mean streak the size of the Mississippi',
+		primary_color: 'wheat',
+		secondary_color: 'white'
+	},
+	{
+		name: 'Two Ton Tony',
+		type: 'cat',
+		owner: 'Jane',
+		personality: 'Scrawny and boney, compared to a hippo',
+		primary_color: 'orange',
+		secondary_color: 'cornsilk'
+	},	
+];
