1c8bae3eb14bb70cd5a142c98e783f5bcbb8cdb5
Sean Hong <writeseanhong@gmail.com>
2016-08-11 20:32:05 -0700

    ang_servCreation

--- ang_services_creation/script.js
+++ ang_services_creation/script.js
@@ -1,32 +1,41 @@
-var app = angular.module('sgtApp', []);
-
-app.provider('sgtData', function(){
-    //Create a variable to hold this
-
-    //Create a variable to hold your api key but set it to an empty string
-
-    //Create a variable to hold the API url but set it to an empty string
-
-
-    //Add the necessary services to the function parameter list
-    this.$get = function(){
-        //return an object that contains a function to call the API and get the student data
-        //Refer to the prototype instructions for more help
-
+var app = angular.module('sgt_app', []);
+app.controller('studentController', function ($scope, $http, getHttp) {
+    var self = this;
+    self.student = {};
+    self.student_array = [];
+    self.get_student_data = function () {
+        req = {
+            url: 'http://s-apis.learningfuze.com/sgt/get',
+            method: 'post',
+            data: $.param({'api_key': 'lN3gVYfP6x'})
+        };
+        student_data = getHttp.get_student_data(req);
+        student_data.then(function (serverdata) {
+            self.student_array = serverdata.data;
+            console.log('its working for get student', self.student_array);
+        }, function () {
+            console.error('its NOT WORKING')
+        }).catch(function () {
+            console.error('FAILURE')
+        });
     };
 });
 
-//Config your provider here to set the api_key and the api_url
+app.factory('getHttp', function($http,$q){
+    var defer = $q.defer();
+    var service = {};
 
-
-//Include your service in the function parameter list along with any other services you may want to use
-app.controller('ioController', function(){
-    //Create a variable to hold this, DO NOT use the same name you used in your provider
-
-    //Add an empty data object to your controller, make sure to call it 'data'
-
-
-    //Add a function called getData to your controller to call the SGT API
-    //Refer to the prototype instructions for more help
-
+    service.get_student_data = function(req) {
+        $http(req).then(function (response) {
+            data = response.data;
+            defer.resolve(data);
+        }).catch(function(response){
+            console.log('the request failed.')
+        });
+        return defer.promise;
+    };
+    return service;
+});
+app.config(function($httpProvider){
+    $httpProvider.defaults.headers.post = {'Content-Type': 'application/x-www-form-urlencoded'};
 });
