a2f8ce9779162325b3da25917645c32a4ce93f0d
Sean Hong <writeseanhong@gmail.com>
2016-08-11 19:53:18 -0700

    Angular Routes - Sean Hong

--- ang_routes/script.js
+++ ang_routes/script.js
@@ -1,22 +1,46 @@
 // Create the route module and name it routeApp
-
+var app = angular.module("routeApp",['ngRoute']);
 // Config the routes
-
-         // route for the home page
-
-        // route for the about page
-
-        // route for the contact page
-
-
-// Create the controllers for the different pages below
-
-// home page controller
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: "pages/home.html",
+            // route for the home page
+            controller: 'mainController'
+        })
+        .when('/about',{
+            templateUrl: "pages/about.html",
+            // route for the about page
+            controller: 'aboutController'
+        })
+        .when('/contact',{
+            templateUrl: "pages/contact.html",
+            // route for the contact page
+            controller: 'contactController'
+        })
+        .otherwise({
+           redirectTo: '/'
+        });
+    // Create the controllers for the different pages below
+}).controller('mainController',function($scope){
+    // home page controller
     // Create a message to display in the view
-
-
-// about page controller
+    $scope.message = "HEY WELCOME HOME";
+}).controller('aboutController',function($scope) {
+    // about page controller
     // Create a message to display in the view
-
-// contact page controller
+    $scope.message = "LEARN MORE ABOUT ME";
+}).controller('contactController',function($scope) {
+    // contact page controller
     // Create a message to display in the view
+    $scope.message = "CALL ME!";
+});
+
+
+
+
+
+
+
+
+
