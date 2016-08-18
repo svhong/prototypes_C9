5f67485f5fce32baf35dc9a4e0125051be86bc56
Sean Hong <writeseanhong@gmail.com>
2016-08-18 11:01:16 -0700

    php_includes - Sean Hong

---
+++ php_includes/main.php
@@ -0,0 +1,6 @@
+<main id="main_content">
+    <a href="#"><img src="includes/emmettbrown.jpg"></a>
+    <br>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque labore perferendis quam repellendus soluta, tempore? Ab ad aliquid amet, animi asperiores aspernatur eos facilis fugiat illo illum nulla numquam odio officia optio praesentium quam, quisquam ratione reiciendis reprehenderit temporibus! Accusamus consectetur debitis id laboriosam molestias nam similique veritatis voluptatem. Veniam.
+
+</main>
