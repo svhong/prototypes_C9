f7806aa666feb5e11b5452962542e16fdb49a52f
Sean Hong <writeseanhong@gmail.com>
2016-08-24 18:04:30 -0700

    php_user_auth - Sean Hong

--- php_user_auth_basic/login_form.php
+++ php_user_auth_basic/login_form.php
@@ -1,1 +1,37 @@
 <!--login_form.php-->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script>
+        function call(){
+            $.ajax({
+                url:'login_handler.php',
+                data:{username:$('.user').val(),
+                    password:$('.pass').val()},
+                cache: 'false',
+                method:'post',
+                dataType:'json',
+                success:function(response){
+                        console.log(response);
+                     $('.response').append(response.message);
+                     $('.response').append(response.error);
+                }
+            });
+
+        }
+    </script>
+</head>
+<body>
+    <input type="text" class="user" placeholder="username">
+    <br>
+    <input type="text" class="pass" placeholder="password">
+    <button onclick="call()">Log In</button>
+    <div class="response"></div>
+</body>
+</html>
--- php_user_auth_basic/login_handler.php
+++ php_user_auth_basic/login_handler.php
@@ -1,1 +1,26 @@
-<!--login_handler.php-->
+<?php
+session_start();
+$_SESSION['username'] = $_POST['username'];
+$_SESSION['password'] = sha1($_POST['password']);
+
+$users = [
+    ['id' => 1, 'username'=>'shong', 'password'=>'39ccb32d95edfdbcd882f2b01809724ec640ea16'],
+    ['id' => 2, 'username'=>'jbyun', 'password'=>'74836f022061ec4ea056a2fc16c7b711b1289c9e'],
+    ['id' => 3, 'username'=>'kngo', 'password'=>'81b06facd90fe7a6e9bbd9cee59736a79105b7be'],
+    ['id' => 4, 'username'=>'bphan', 'password'=>'de163f3b3a5630ec6ed8c90a2288aec953ce809a'],
+    ['id' => 5, 'username'=>'ealfano', 'password'=>'2aed5404c83f7a46aa249e0a6328af756b19d513']
+];
+$output['success'] = false;
+foreach($users as $u) {
+    if ($_SESSION['username'] == $u['username'] && $_SESSION['password'] == $u['password']) {
+        $output['success'] = true;
+    }
+}
+if($output['success']){
+    $_SESSION['user_id'] = $u['id'];
+    $output['message'] = 'User Successfully Logged in!';
+} else {
+    $output['error'] = 'Invalid username and password';
+}
+print(json_encode($output));
+?>
